4a92e44ed18107b93e1ea9e44340e886d2146618
AndreyLinnik <av.linnik79@ya.ru>
2022-08-06 20:05:11 +0300

    homework

---
+++ homeWork/task4/task4.cs
@@ -0,0 +1,11 @@
+﻿onsole.Write("введите число a=");
+
+int number = int.Parse(Console.ReadLine());
+Console.Write("введите число b=");
+
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("введите число c=");
+int number2 = int.Parse(Console.ReadLine());
+int max;
+max = Math.Max(number, Math.Max(number1, number2));
+Console.WriteLine(max);
---
+++ homeWork/task6/task6.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("число=");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine(num1 % 2 == 0 ? "число четное" : "число не четное");
