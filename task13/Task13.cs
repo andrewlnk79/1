cea47b683b0584cd9b6ece0a5aa7d074ba2a6cf3
AndreyLinnik <av.linnik79@ya.ru>
2022-08-12 10:20:25 +0300

    task13

---
+++ task13/Task13.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Threading.Channels;
+
+int number=new Random().Next(1,200);
+Console.WriteLine("ввели цифру="+number);
+if (number / 100 > 0)
+{
+    Console.WriteLine("третья цифра числа это:"+(number/100%10));
+}
+Console.WriteLine("у числа нет 3ей цифры");
