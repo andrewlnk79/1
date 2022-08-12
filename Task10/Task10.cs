38d3f3d967783c14385ea56dbd18cf40615facd3
AndreyLinnik <av.linnik79@ya.ru>
2022-08-12 11:25:53 +0300

    edit mistake

--- Task10/Task10.cs
+++ Task10/Task10.cs
@@ -1,5 +1,5 @@
 ﻿int number=new Random().Next(10,999);
-Console.WriteLine("ввели цифру="+number);
+Console.WriteLine("ввели число="+number);
 if (number / 100 > 0)
 {
     Console.WriteLine("вторая цифра числа это:"+(number/10%10));
