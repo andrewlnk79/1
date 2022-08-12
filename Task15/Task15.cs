4d6753a05178ca1ba21399853c92808798fc85d7
AndreyLinnik <av.linnik79@ya.ru>
2022-08-12 14:11:22 +0300

    task15

---
+++ Task15/Task15.cs
@@ -0,0 +1,6 @@
+﻿int dayNum = new Random().Next(1, 7);
+String[] daysOfTheWeek ={"monday","tusday","wednesday","thursday","friday","suturday","sunday"};
+if (dayNum > 5){
+Console.WriteLine("выходной");
+};
+Console.WriteLine("пока работаем");
