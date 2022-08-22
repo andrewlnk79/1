2b5095b3842eb0ba12b2e1c556a15be5a2348d4d
AndreyLinnik <av.linnik79@ya.ru>
2022-08-22 16:55:46 +0300

    lesson5

---
+++ task34/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] CrateArray(int size, int min, int value)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(min, value + 1);
+        
+    }
+
+    return arr;
+
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine(String.Join(" ",array));
+}
+
+int[] arr1 = CrateArray(20, 200, 700);
+PrintArray(arr1);
